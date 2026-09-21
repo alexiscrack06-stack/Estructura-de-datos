fe6626ad9d4251d7ebc67dce40f74c5550c4f921
Noel Alexis <alexiscrack06@gmail.com>
2026-09-20 18:56:55 -0700

    Add files via upload

---
+++ python/recorridoSecuencial.py
@@ -0,0 +1,10 @@
+# Declaración del array (lista)
+numeros = [10, 20, 30, 40, 50]
+
+# Recorrido por elementos
+for num in numeros:
+    print(f"Elemento: {num}")
+
+# Recorrido por índice
+for i in range(len(numeros)):
+    print(f"Índice {i}: {numeros[i]}")
