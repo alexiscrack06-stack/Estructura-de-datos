eb0f6b7a6b06af97c006b40597719cc6557c6dd2
Noel Alexis <alexiscrack06@gmail.com>
2026-09-20 19:34:21 -0700

    Add files via upload

---
+++ c#/recorrido2D.cs
@@ -0,0 +1,21 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[,] matriz = {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 }
+        };
+
+        for (int i = 0; i < matriz.GetLength(0); i++)
+        {
+            for (int j = 0; j < matriz.GetLength(1); j++)
+            {
+                Console.WriteLine($"[{i}][{j}] = {matriz[i, j]}");
+            }
+        }
+    }
+}
